0104786eb13bc61a2752e81938f667de348831de
charleshuber <charles.huber@hotmail.fr>
2018-10-14 15:09:00 +0200

    sigmoid

---
+++ Chapter3/lib_scikitlearn.py
@@ -0,0 +1,38 @@
+import numpy as np
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+def plot_decision_regions(X, y, classifier, plt, test_idx=None, resolution=0.01):
+
+    #setup marker generator and color map
+    markers = ('s','x','o','^','v')
+    colors = ('red','blue','lightgreen','gray','cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    xx1, xx2 = np.meshgrid(
+        np.arange(x1_min, x1_max, resolution),
+        np.arange(x2_min, x2_max, resolution))
+
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    print('Z:', Z)
+    Z = Z.reshape(xx1.shape)
+    print('Z reshape:', Z)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y==cl, 0], y=X[y==cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+    # highlight test samples
+    if test_idx:
+        X_test, y_test = X[test_idx,:], y[test_idx]
+        plt.scatter(X_test[:,0], X_test[:,1],c='black',
+                    alpha=0.2, linewidths=2, marker='o',
+                    s=55, label='test set')
--- Chapter3/training_perceptron.py
+++ Chapter3/training_perceptron.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 from sklearn import datasets
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
+from lib_scikitlearn import plot_decision_regions
 
 iris = datasets.load_iris()
 X = iris.data[:,[2,3]]
@@ -25,3 +27,12 @@ y_pred = ppn.predict(X_test_std)
 
 print('Misclassified samples: %d'%(y_test != y_pred).sum(), '/%d' % len(y_test))
 print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn, plt=plt, test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
